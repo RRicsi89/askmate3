d807de9f002adb6f703699b212e31981010676fb
Tóth Gabriella <thgabi1@gmail.com>
2022-03-23 11:16:14 +0100

    Changes by qephetzial

--- server.py
+++ server.py
@@ -52,7 +52,7 @@ def add_answer(question_id=None):
     if request.method == "GET":
         question = connections.get_data(question_id, connections.QUESTIONS_PATH)
         return render_template("new_answer.html", question=question)
-    if request.method == "POST":
+    elif request.method == "POST":
         message = request.form['message']
         new_answer = {"id": utils.generate_uuid(), "submission_time": utils.get_time(), "vote_number": 0, "question_id": question_id, "message": message}
         for key, value in request.form.items():
@@ -67,10 +67,22 @@ def add_answer(question_id=None):
 @app.route("/question/<question_id>/edit", methods=["GET", "POST"])
 def edit(question_id=None):
     question = connections.get_data(question_id, connections.QUESTIONS_PATH)
-    time=utils.get_time()
-    return render_template(add_question.html, question=question[5], titles=question[4])
+    if request.method =='GET':
+        return render_template("edit_question.html", question=question, question_id=question_id)
+    if request.method == "POST":
+        edited_title = request.form["title"]
+        edited_message = request.form["message"]
 
+        question = {"id": question_id, "submission_time": utils.get_time(), "view_number": 0, "vote_number":0, "title": edited_title, "message": edited_message}
 
+        image = request.files["image"]
+        if image:
+            image.save(os.path.join(connections.IMAGE_FOLDER_PATH, image.filename))
+            question["image"] = f"images/{image.filename}"
+
+        connections.write_to_file(connections.QUESTIONS_PATH, question, connections.QUESTION_HEADERS_CSV)
+
+        return redirect("/list")
 
 
 if __name__ == "__main__":
