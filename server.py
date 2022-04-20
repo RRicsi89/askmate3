d838d449ce145f7b316dca32026da58e40be356c
Csiga <sablablab@gmail.com>
2022-04-20 10:17:14 +0200

    Move login/logout to navbar

--- server.py
+++ server.py
@@ -13,9 +13,13 @@ app.permanent_session_lifetime = timedelta(minutes=20)
 
 @app.route("/")
 def hello():
-
-    latest_five_question = data_handler.get_latest_five_question()
-    return render_template("index.html", question_data=latest_five_question)
+    if 'email' in session:
+        latest_five_question = data_handler.get_latest_five_question()
+        return render_template("index.html", question_data=latest_five_question, email=session['email'],
+                               user_id=session['user_id'])
+    else:
+        latest_five_question = data_handler.get_latest_five_question()
+        return render_template("index.html", question_data=latest_five_question)
 
 
 @app.route("/list")
