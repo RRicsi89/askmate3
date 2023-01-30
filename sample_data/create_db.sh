dfd566e65ff77c5fc2dbb2f43e0870c0e8ab37fa
RRicsi89 <rozsar1989@gmail.com>
2023-01-30 15:30:41 +0100

    Add files to create database

---
+++ sample_data/create_db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
+	CREATE DATABASE askmate;
+	GRANT ALL PRIVILEGES ON DATABASE askmate TO postgres;
+    \c askmate
+    \i askmatepart2-sample-data.sql;
+EOSQL
