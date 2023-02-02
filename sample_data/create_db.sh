a99bc43cbce2f49da48d5483f91f7f6862814e45
RRicsi89 <rozsar1989@gmail.com>
2023-02-02 17:18:11 +0100

    Revert database config files

--- sample_data/create_db.sh
+++ sample_data/create_db.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-if [[ "$(ls -A /var/lib/postgresql/data/pgdata)" ]]; then
-    printf "Found database"
-    cp -r /var/lib/postgresql/data/pgdata/* /var/lib/postgresql/data
-else
+# if [[ "$(ls -A /var/lib/postgresql/data/pgdata)" ]]; then
+#     printf "Found database"
+#     cp -r /var/lib/postgresql/data/pgdata/* /var/lib/postgresql/data
+# else
 psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
     CREATE DATABASE askmate;
     GRANT ALL PRIVILEGES ON DATABASE askmate TO postgres;
     \c askmate
     \i askmatepart2-sample-data.sql;
 EOSQL
-fi
+# fi
