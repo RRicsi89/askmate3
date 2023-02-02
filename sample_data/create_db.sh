4766cf9bee3d8628f2302e61c58c1b6df4ab33b3
RRicsi89 <rozsar1989@gmail.com>
2023-02-02 17:06:05 +0100

    Update db init file

--- sample_data/create_db.sh
+++ sample_data/create_db.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-if [[ "$(ls -A /data)" ]]; then
+if [[ "$(ls -A /var/lib/postgresql/data/pgdata)" ]]; then
     printf "Found database"
-    cp -r /var/lib/postgresql/data/pgdata /var/lib/postgresql/data
+    cp -r /var/lib/postgresql/data/pgdata/ /var/lib/postgresql/data
 else
 psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
     CREATE DATABASE askmate;
