05fab8430ffbd401d9f5a296d3d9fadd3d687561
RRicsi89 <rozsar1989@gmail.com>
2023-02-02 14:34:12 +0100

    Update database config to use persistent data

--- sample_data/create_db.sh
+++ sample_data/create_db.sh
@@ -2,9 +2,14 @@
 
 set -e
 
+if [[ "$(ls -A /data)" ]]; then
+    printf "Found database"
+    cp -r /data /var/lib/postgresql/data
+else
 psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
-	CREATE DATABASE askmate;
-	GRANT ALL PRIVILEGES ON DATABASE askmate TO postgres;
+    CREATE DATABASE askmate;
+    GRANT ALL PRIVILEGES ON DATABASE askmate TO postgres;
     \c askmate
     \i askmatepart2-sample-data.sql;
 EOSQL
+fi
