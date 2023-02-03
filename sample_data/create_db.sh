792f395ddd58b2f836e4f89d0db7399435b9d999
RRicsi89 <rozsar1989@gmail.com>
2023-02-03 10:35:32 +0100

    Update PV reclaim policy and database config

--- sample_data/create_db.sh
+++ sample_data/create_db.sh
@@ -2,13 +2,9 @@
 
 set -e
 
-if [[ "$(ls -A /var/lib/postgresql/data/pgdata)" ]]; then
-    printf "Found database"
-else
 psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
     CREATE DATABASE askmate;
     GRANT ALL PRIVILEGES ON DATABASE askmate TO postgres;
     \c askmate
     \i askmatepart2-sample-data.sql;
 EOSQL
-fi
